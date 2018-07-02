64822c461076239a0674c543f8a373439d692796
Caleb <33265183+cmmitchell@users.noreply.github.com>
2018-07-02 15:02:25 -0400

    Update caro_point.js

--- Location/Shapefile/caro_point.js
+++ Location/Shapefile/caro_point.js
@@ -4,7 +4,7 @@ var points =
 "name": "caro_point",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "Name": "Cavalry Baptist Church", "Use": "Main Location" }, "geometry": { "type": "Point", "coordinates": [ -75.816072592980831, 38.882555467825178 ] } },
+{ "type": "Feature", "properties": { "id": 1, "Name": "Calvary Baptist Church", "Use": "Main Location" }, "geometry": { "type": "Point", "coordinates": [ -75.816072592980831, 38.882555467825178 ] } },
 { "type": "Feature", "properties": { "id": 2, "Name": "Crane Baptist Park", "Use": "Park" }, "geometry": { "type": "Point", "coordinates": [ -75.855916680908265, 38.840480651950308 ] } }
 ]
 }
