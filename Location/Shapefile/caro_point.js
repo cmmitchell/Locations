3b5df254811787df0ae390d9b21970acb06bbd61
Caleb <33265183+cmmitchell@users.noreply.github.com>
2018-07-02 14:27:43 -0400

    Add files via upload

---
+++ Location/Shapefile/caro_point.js
@@ -0,0 +1,10 @@
+var points =
+{
+"type": "FeatureCollection",
+"name": "caro_point",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Name": "Cavalry Baptist Church", "Use": "Main Location" }, "geometry": { "type": "Point", "coordinates": [ -75.816072592980831, 38.882555467825178 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Name": "Crane Baptist Park", "Use": "Park" }, "geometry": { "type": "Point", "coordinates": [ -75.855916680908265, 38.840480651950308 ] } }
+]
+}
