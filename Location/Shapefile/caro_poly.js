d94a3c770079f17f552af2b1711f4722ef61aeb0
Caleb <33265183+cmmitchell@users.noreply.github.com>
2018-07-10 10:41:53 -0400

    Update caro_poly.js

--- Location/Shapefile/caro_poly.js
+++ Location/Shapefile/caro_poly.js
@@ -24,7 +24,7 @@ function polycol2(feature) {
         fillColor: "#58c712",
         weight: 2,
         opacity: 1,
-        color: "#d05e00", weight: 5,
+        color: "#d05e00", weight: 3,
         dashArray: '5',
         fillOpacity: 0
     };
