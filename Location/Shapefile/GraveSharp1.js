3f0b5654f5e2572deb2bb9d8507e2ba1415a17c4
Caleb <33265183+cmmitchell@users.noreply.github.com>
2019-12-09 18:24:25 -0500

    Add files via upload

---
+++ Location/Shapefile/GraveSharp1.js
@@ -0,0 +1,63 @@
+var gravesharp =
+{
+"type": "FeatureCollection",
+"name": "GraveSharp3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.57364, "Long": -75.79241 }, "geometry": { "type": "Point", "coordinates": [ 38.57364, -75.79241 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.57214, "Long": -75.79216 }, "geometry": { "type": "Point", "coordinates": [ 38.57214, -75.79216 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5739, "Long": -75.8 }, "geometry": { "type": "Point", "coordinates": [ 38.5739, -75.8 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5758, "Long": -75.796 }, "geometry": { "type": "Point", "coordinates": [ 38.5758, -75.796 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5873, "Long": -75.7935 }, "geometry": { "type": "Point", "coordinates": [ 38.5873, -75.7935 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5881, "Long": -75.797 }, "geometry": { "type": "Point", "coordinates": [ 38.5881, -75.797 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.585, "Long": -75.7888 }, "geometry": { "type": "Point", "coordinates": [ 38.585, -75.7888 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.585, "Long": -75.7888 }, "geometry": { "type": "Point", "coordinates": [ 38.585, -75.7888 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.6069, "Long": -75.7844 }, "geometry": { "type": "Point", "coordinates": [ 38.6069, -75.7844 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.55868, "Long": -75.76585 }, "geometry": { "type": "Point", "coordinates": [ 38.55868, -75.76585 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.57369, "Long": -75.73596 }, "geometry": { "type": "Point", "coordinates": [ 38.57369, -75.73596 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5658, "Long": -75.7137 }, "geometry": { "type": "Point", "coordinates": [ 38.5658, -75.7137 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5594, "Long": -75.7151 }, "geometry": { "type": "Point", "coordinates": [ 38.5594, -75.7151 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.56816, "Long": -75.71225 }, "geometry": { "type": "Point", "coordinates": [ 38.56816, -75.71225 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.59086, "Long": -75.71217 }, "geometry": { "type": "Point", "coordinates": [ 38.59086, -75.71217 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.60194, "Long": -75.70071 }, "geometry": { "type": "Point", "coordinates": [ 38.60194, -75.70071 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.61166, "Long": -75.70392 }, "geometry": { "type": "Point", "coordinates": [ 38.61166, -75.70392 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.61564, "Long": -75.69684 }, "geometry": { "type": "Point", "coordinates": [ 38.61564, -75.69684 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.54073, "Long": -75.72008 }, "geometry": { "type": "Point", "coordinates": [ 38.54073, -75.72008 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.53925, "Long": -75.71771 }, "geometry": { "type": "Point", "coordinates": [ 38.53925, -75.71771 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.53285, "Long": -75.71796 }, "geometry": { "type": "Point", "coordinates": [ 38.53285, -75.71796 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.53221, "Long": -75.71535 }, "geometry": { "type": "Point", "coordinates": [ 38.53221, -75.71535 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.52855, "Long": -75.71336 }, "geometry": { "type": "Point", "coordinates": [ 38.52855, -75.71336 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.52493, "Long": -75.72595 }, "geometry": { "type": "Point", "coordinates": [ 38.52493, -75.72595 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.52358, "Long": -75.73585 }, "geometry": { "type": "Point", "coordinates": [ 38.52358, -75.73585 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5133, "Long": -75.7437 }, "geometry": { "type": "Point", "coordinates": [ 38.5133, -75.7437 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5114, "Long": -75.7527 }, "geometry": { "type": "Point", "coordinates": [ 38.5114, -75.7527 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4931, "Long": -75.7548 }, "geometry": { "type": "Point", "coordinates": [ 38.4931, -75.7548 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4937, "Long": -75.7654 }, "geometry": { "type": "Point", "coordinates": [ 38.4937, -75.7654 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4869, "Long": -75.7679 }, "geometry": { "type": "Point", "coordinates": [ 38.4869, -75.7679 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4883, "Long": -75.7762 }, "geometry": { "type": "Point", "coordinates": [ 38.4883, -75.7762 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4938, "Long": -75.7262 }, "geometry": { "type": "Point", "coordinates": [ 38.4938, -75.7262 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.4935, "Long": -75.7137 }, "geometry": { "type": "Point", "coordinates": [ 38.4935, -75.7137 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5157, "Long": -75.7198 }, "geometry": { "type": "Point", "coordinates": [ 38.5157, -75.7198 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.49353, "Long": -75.71369 }, "geometry": { "type": "Point", "coordinates": [ 38.49353, -75.71369 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.48829, "Long": -75.72127 }, "geometry": { "type": "Point", "coordinates": [ 38.48829, -75.72127 ] } },
+{ "type": "Feature", "properties": { "Known": "Q", "Lat": 38.5309, "Long": -75.71325 }, "geometry": { "type": "Point", "coordinates": [ 38.5309, -75.71325 ] } },
+{ "type": "Feature", "properties": { "Known": "Q", "Lat": 38.5729, "Long": -75.7178 }, "geometry": { "type": "Point", "coordinates": [ 38.5729, -75.7178 ] } },
+{ "type": "Feature", "properties": { "Known": "Q", "Lat": 38.53722, "Long": -75.71079 }, "geometry": { "type": "Point", "coordinates": [ 38.53722, -75.71079 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.6307, "Long": -75.81295 }, "geometry": { "type": "Point", "coordinates": [ 38.6307, -75.81295 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.63751, "Long": -75.78142 }, "geometry": { "type": "Point", "coordinates": [ 38.63751, -75.78142 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5961, "Long": -75.7615 }, "geometry": { "type": "Point", "coordinates": [ 38.5961, -75.7615 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.64262, "Long": -75.77194 }, "geometry": { "type": "Point", "coordinates": [ 38.64262, -75.77194 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.64957, "Long": -75.76112 }, "geometry": { "type": "Point", "coordinates": [ 38.64957, -75.76112 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.5877, "Long": -75.7758 }, "geometry": { "type": "Point", "coordinates": [ 38.5877, -75.7758 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.61749, "Long": -75.72571 }, "geometry": { "type": "Point", "coordinates": [ 38.61749, -75.72571 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.61426, "Long": -75.72842 }, "geometry": { "type": "Point", "coordinates": [ 38.61426, -75.72842 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.6191, "Long": -75.74304 }, "geometry": { "type": "Point", "coordinates": [ 38.6191, -75.74304 ] } },
+{ "type": "Feature", "properties": { "Known": "K", "Lat": 38.63545, "Long": -75.71628 }, "geometry": { "type": "Point", "coordinates": [ 38.63545, -75.71628 ] } }
+]
+}
+
+function GraveCol(feature) {
+	switch (feature.properties.Type) {
+		default: return {radius: 1.25, fillColor: "#0099FF", weight: 1, fillOpacity: 1, color: "#000000", weight: 1, opacity: 1};
+   };
+}
